cc00c3e6d79020aa08110dc3548b468678ca81db
Sparrow-33 <elhaoudihaytham2001@gmail.com>
2022-03-29 13:48:08 +0000

    countries selection

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="grid md:grid-cols-2 gap-4">
+    <!-- Cases Box -->
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">
+        Cases
+      </h3>
+
+      <div class="text-2xl mb-4">
+        <span class="font-bold">
+          New :
+        </span>
+        {{addCommas(status.NewConfirmed)}}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">
+          Total :
+        </span>
+        {{addCommas(status.TotalConfirmed)}}
+      </div>
+    </div>
+
+    <!-- Death Box -->
+    <div class="shadow-md bg-blue-200 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">
+        Deaths
+      </h3>
+
+      <div class="text-2xl mb-4">
+        <span class="font-bold">
+          New :
+        </span>
+        {{addCommas(status.NewDeaths)}}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">
+          Total :
+        </span>
+        {{addCommas(status.TotalDeaths)}}
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataBoxes",
+  props:['status'],
+  methods:{
+    addCommas(x){
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,22 @@
 <template>
   <main v-if="!loading">
-Show Data
+    <data-title :text="title"
+                :data-date="dataDate"
+    />
+    <data-boxes :status="status"/>
+
+    <SelectCountry @get-country="getCountryData"
+                   :countries="countries"/>
+
+    <button @click="clearCountryData"
+          v-if="status.Country"
+          class="bg-green-600
+                   text-white
+                   rounded p-3 mt-10 mb-10
+                   focus:outline-none
+                   hover:bg-green-500">
+     Clear Country
+    </button>
   </main>
 
   <main v-else class="flex flex-col align-center justify-center text-center" >
@@ -12,11 +28,19 @@ Show Data
 </template>
 
 <script>
+import dataTitle from "@/components/DataTitle";
+import dataBoxes from "@/components/DataBoxes";
+import SelectCountry from "@/components/SelectCountry";
 export default {
   name: 'HomePage',
+  components:{
+    dataTitle,
+    dataBoxes,
+    SelectCountry
+  },
   data(){
    return {
-     loading : true,
+     loading : false,
      title: 'Global',
      dataDate: '',
      status:{},
@@ -32,6 +56,17 @@ export default {
   const data = await responce.json()
     return data
   },
+     getCountryData(country){
+       this.status = country
+       this.title = country.Country
+    },
+   async clearCountryData(){
+       this.loading = true
+       const data = await this.fetchCovidData()
+       this.title = 'Global'
+      this.status = data.Global
+     this.loading =false
+    }
   },
 async created() {
     const data = await this.fetchCovidData()
